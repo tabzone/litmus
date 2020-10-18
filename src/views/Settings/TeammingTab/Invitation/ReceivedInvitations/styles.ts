598aa3e01ee16f9aad7d11460665d388aeabd060
Tabzone <tabrez@gmail.com>
2020-10-17 22:03:21 -0700

    Initial commit

---
+++ src/views/Settings/TeammingTab/Invitation/ReceivedInvitations/styles.ts
@@ -0,0 +1,44 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    maxHeight: '20.1875rem',
+    marginBottom: theme.spacing(3),
+  },
+  avatarBackground: {
+    backgroundColor: theme.palette.totalRunsCountColor,
+    width: '2.56rem',
+    height: '2.56rem',
+    color: theme.palette.customColors.white(1),
+    marginRight: theme.spacing(2.5),
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: theme.spacing(2.5),
+    },
+  },
+  nameRole: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  role: {
+    borderRadius: '0.1875rem',
+    color: theme.palette.secondary.dark,
+    fontSize: '0.75rem',
+    marginLeft: theme.spacing(1.0),
+  },
+  rowDiv: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  firstCol: {
+    display: 'flex',
+  },
+  detail: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(0.5),
+  },
+  buttonDiv: {
+    display: 'flex',
+  },
+}));
+export default useStyles;
---
+++ src/views/Settings/UserManagementTab/EditUser/DelUser/index.tsx
@@ -0,0 +1,80 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import ButtonFilled from '../../../../../components/Button/ButtonFilled';
+import ButtonOutline from '../../../../../components/Button/ButtonOutline';
+import Unimodal from '../../../../../containers/layouts/Unimodal';
+import useStyles from './styles';
+// props for DelUser component
+interface DelUserProps {
+  handleModal?: () => void;
+  tableDelete: boolean;
+  handleTable: () => void;
+  teammingDel: boolean;
+  disabled?: boolean;
+}
+
+// DelUser displays the modal for deteing a user
+const DelUser: React.FC<DelUserProps> = ({
+  handleModal,
+  tableDelete,
+  teammingDel,
+  handleTable,
+}) => {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const { t } = useTranslation();
+
+  const handleClose = () => {
+    handleTable();
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Unimodal isOpen={open} handleClose={handleClose} hasCloseBtn>
+        <div className={classes.body}>
+          <img src="./icons/userDel.svg" alt="lock" />
+          <div className={classes.text}>
+            <Typography className={classes.typo} align="center">
+              {t('settings.teamingTab.deleteModal.header')}
+              <strong> {t('settings.teamingTab.deleteModal.text')}</strong>
+            </Typography>
+          </div>
+          <div className={classes.textSecond}>
+            <Typography className={classes.typoSub} align="center">
+              {teammingDel ? (
+                <>{t('settings.teamingTab.deleteModal.body')}</>
+              ) : (
+                <>
+                  {t('settings.userManagementTab.deleteUser.deleteModal.body')}
+                </>
+              )}
+            </Typography>
+          </div>
+          <div className={classes.buttonGroup}>
+            <ButtonOutline
+              isDisabled={false}
+              handleClick={() => {
+                setOpen(false);
+              }}
+            >
+              <> {t('settings.teamingTab.deleteModal.noButton')}</>
+            </ButtonOutline>
+
+            <ButtonFilled
+              isDisabled={false}
+              isPrimary
+              handleClick={tableDelete ? handleModal : handleClose}
+            >
+              <>{t('settings.teamingTab.deleteModal.yesButton')}</>
+            </ButtonFilled>
+          </div>
+        </div>
+      </Unimodal>
+    </div>
+  );
+};
+export default DelUser;
